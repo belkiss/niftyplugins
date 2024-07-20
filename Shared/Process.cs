ff5ef9f55340dd597d42db5b19ea54096bd44c75
Lambert Clara <lambert.clara@yahoo.fr>
2024-07-20 16:33:52 +0200

    Update copyright year to 2024

--- Shared/Process.cs
+++ Shared/Process.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (C) 2006-2017 Jim Tilander, 2017-2023 Lambert Clara. See the COPYING file in the project root for full license information.
+﻿// Copyright (C) 2006-2017 Jim Tilander, 2017-2024 Lambert Clara. See the COPYING file in the project root for full license information.
 
 using System;
 using System.Globalization;
