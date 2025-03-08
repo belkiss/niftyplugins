942b769ad6ecf3d04c4e79b711fb9d5af3495568
Lambert Clara <lambert.clara@yahoo.fr>
2025-03-08 18:08:08 +0100

    Remove unused using

--- NiftyPerforce/P4Operations.cs
+++ NiftyPerforce/P4Operations.cs
@@ -7,7 +7,6 @@ using System.Globalization;
 using System.IO;
 using System.Text.RegularExpressions;
 using Aurora;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace NiftyPerforce
 {
