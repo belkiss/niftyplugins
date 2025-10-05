fd837f4c7a56f71a40f03b1c2db6af06feb9aa3e
Lambert Clara <lambert.clara@yahoo.fr>
2025-10-05 16:18:25 +0200

    Add new class holding p4 connection info (unused for now)

---
+++ NiftyPerforce.Core/P4ConnectionInfo.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (C) 2006-2017 Jim Tilander, 2017-2025 Lambert Clara. See the COPYING file in the project root for full license information.
+
+using System.Collections.Generic;
+
+namespace NiftyPerforce.Core
+{
+    public class P4ConnectionInfo
+    {
+        public string? Client { get; set; }
+
+        public string? Server { get; set; }
+
+        public string? Username { get; set; }
+
+        public bool IsValid() => !string.IsNullOrEmpty(Server) && !string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Client);
+
+        public string? ConnectionString => $"-p {Server} -u {Username} -c {Client}";
+
+        public Dictionary<string, string> GetEnv() => new Dictionary<string, string>()
+        {
+            { "P4CLIENT", Client ?? string.Empty },
+            { "P4PORT", Server ?? string.Empty },
+            { "P4USER", Username ?? string.Empty },
+        };
+    }
+}
