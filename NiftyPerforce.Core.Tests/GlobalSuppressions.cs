5efa8a196dc2a39e56c44e7fb9dffa9e019cdaf4
Lambert Clara <lambert.clara@yahoo.fr>
2025-11-14 17:34:59 +0100

    Bump Microsoft.NET.Test.Sdk and MSTest versions, and fix check by adding parallelize attribute

--- NiftyPerforce.Core.Tests/GlobalSuppressions.cs
+++ NiftyPerforce.Core.Tests/GlobalSuppressions.cs
@@ -1,5 +1,8 @@
 ﻿// Copyright (C) 2006-2017 Jim Tilander, 2017-2025 Lambert Clara. See the COPYING file in the project root for full license information.
 
 using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
 
 [assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "As recommended upstream, unit-tests do not have to follow this.")]
--- NiftyPerforce.Tests/P4Operations.cs
+++ NiftyPerforce.Tests/P4Operations.cs
@@ -1,5 +1,9 @@
 ﻿// Copyright (C) 2006-2017 Jim Tilander, 2017-2025 Lambert Clara. See the COPYING file in the project root for full license information.
 
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
+
 namespace NiftyPerforce.Tests
 {
 }
